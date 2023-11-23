24d87992c8cf97133b0c1214a273ec24bea283a0
Gianluca Ghislotti <g.ghislotti@campus.unimib.it>
2023-11-23 14:59:30 +0100

    Accelerometer integrated

--- app/src/main/java/com/example/runtime/ui/run/RunFragment.java
+++ app/src/main/java/com/example/runtime/ui/run/RunFragment.java
@@ -1,10 +1,18 @@
 package com.example.runtime.ui.run;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
@@ -13,6 +21,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.runtime.R;
 import com.example.runtime.databinding.FragmentRunBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
 
 public class RunFragment extends Fragment {
 
@@ -24,6 +37,11 @@ public class RunFragment extends Fragment {
     private boolean pauseManagement = true;
     private float heightcm = 0;
     private float stridem = (float) (heightcm * 1.4);
+
+    //Sensor variable managing
+    private Sensor accSensor;
+    private SensorManager sensorManager;
+    private StepCounterListener sensorListener;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -38,6 +56,11 @@ public class RunFragment extends Fragment {
         pause = (ImageButton) root.findViewById(R.id.pause);
         stop = (ImageButton) root.findViewById(R.id.stop);
 
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+
+
         setButtonListener();
 
         return root;
@@ -51,6 +74,9 @@ public class RunFragment extends Fragment {
             pause.setVisibility(View.VISIBLE);
             stop.setVisibility(View.VISIBLE);
             pause.setImageResource(android.R.drawable.ic_media_pause);
+
+            startResumeRun();
+
         });
 
         //setOnClickListener of the Stop ImageButton
@@ -58,8 +84,10 @@ public class RunFragment extends Fragment {
             play.setVisibility(View.VISIBLE);
             pause.setVisibility(View.GONE);
             stop.setVisibility(View.GONE);
+            stopPauseRun();
             pause.setImageResource(android.R.drawable.ic_media_pause);
-            pauseManagement = true;
+            pauseManagement = false;
+
         });
 
         //setOnClickListener of the Pause ImageButton
@@ -67,16 +95,34 @@ public class RunFragment extends Fragment {
             if(pauseManagement){
                 pause.setImageResource(android.R.drawable.ic_media_play);
                 pauseManagement = false;
+                stopPauseRun();
                 //create a run part object and push it in firebase
             }else{
                 pause.setImageResource(android.R.drawable.ic_media_pause);
                 pauseManagement = true;
+                startResumeRun();
             }
         });
-
     }
 
+    //Method used to activate the sensorListener when the user press start or resume buttons
+    public void startResumeRun(){
+        if (accSensor == null) {
+            Log.e("SensorError", "Accelerometer sensor not available");
 
+        }else if(sensorListener == null){
+            sensorListener = new StepCounterListener();
+            sensorManager.registerListener(sensorListener, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    //Method used to stop the stepCount listener when the user press pause or stop buttons
+    public void stopPauseRun(){
+        if (sensorListener != null) {
+            sensorManager.unregisterListener(sensorListener);
+            sensorListener = null;
+        }
+    }
 
         @Override
     public void onDestroyView() {
@@ -84,4 +130,71 @@ public class RunFragment extends Fragment {
         binding = null;
     }
 
+}
+
+
+class  StepCounterListener implements SensorEventListener {
+
+    TextView stepCountsView;
+    public static int accDetectorCounter = 0;
+    private long lastSensorUpdate = 0;
+    List<Integer> accSeries = new ArrayList<>();
+    private double accMag = 0;
+    private int lastAddedIndex = 1;
+    int stepThreshold = 7;
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // Check the type of the sensor, this is helpful in case of multiple sensors
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+
+            //Get the raw acc. sensor data
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            // Computing the magnitude for the acceleration
+            accMag = Math.sqrt(x*x+y*y+z*z);
+
+            //Storing the magnitude for the acceleration in accSeries
+            accSeries.add((int) accMag);
+
+            peakDetection();
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void peakDetection() {
+
+        int windowSize = 10;
+        /* Peak detection algorithm derived from: A Step Counter Service for Java-Enabled Devices Using a Built-In Accelerometer Mladenov et al.
+         */
+        int currentSize = accSeries.size(); // get the length of the series
+        if (currentSize - lastAddedIndex < windowSize) { // if the segment is smaller than the processing window size skip it
+            return;
+        }
+
+        List<Integer> valuesInWindow = accSeries.subList(lastAddedIndex,currentSize);
+        lastAddedIndex = currentSize;
+
+        for (int i = 1; i < valuesInWindow.size()-1; i++) {
+            int forwardSlope = valuesInWindow.get(i + 1) - valuesInWindow.get(i);
+            int downwardSlope = valuesInWindow.get(i) - valuesInWindow.get(i - 1);
+
+            if (forwardSlope < 0 && downwardSlope > 0 && valuesInWindow.get(i) > stepThreshold) {
+                countSteps(accDetectorCounter);
+                Log.d("ACC STEPS: ", String.valueOf(accDetectorCounter));
+
+            }
+        }
+    }
+
+    private void countSteps(float step) {
+        accDetectorCounter += step;
+
+        Log.d("ACCELEROMETER REVELED STEPS: ", String.valueOf(accDetectorCounter));
+    }
 }
